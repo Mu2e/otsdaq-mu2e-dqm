d2bbeaf69d89fb2b502e87f1b561cf62da309065
mmackenz@FNAL.GOV <mmackenz@FNAL.GOV>
2025-04-23 15:47:32 -0500

    clang format

--- otsdaq-mu2e-dqm/ArtModules/SimpleDQM_module.cc
+++ otsdaq-mu2e-dqm/ArtModules/SimpleDQM_module.cc
@@ -36,14 +36,16 @@ class SimpleDQM : public art::EDAnalyzer
 		    Name("address"),
 		    Comment("This paramter sets the IP address where the "
 		            "histogram will be sent")};
-		fhicl::Atom<std::string> outputTag{Name("outputTag"), Comment("Tag to use in output name")};
-		fhicl::Atom<int> freqDQM{
-		    Name("freqDQM"),
-		    Comment("Frequency for sending histograms to the data-receiver")};
-		fhicl::Atom<int> diag{Name("diagLevel"), Comment("Diagnostic level"), 0};
+		fhicl::Atom<std::string> outputTag{Name("outputTag"),
+		                                   Comment("Tag to use in output name")};
+		fhicl::Atom<int>         freqDQM{
+            Name("freqDQM"),
+            Comment("Frequency for sending histograms to the data-receiver")};
+		fhicl::Atom<int>  diag{Name("diagLevel"), Comment("Diagnostic level"), 0};
 		fhicl::Atom<bool> sendHists{
 		    Name("sendHists"),
-		    Comment("Whether or not to send histograms to the receiver"), true};
+		    Comment("Whether or not to send histograms to the receiver"),
+		    true};
 	};
 
 	typedef art::EDAnalyzer::Table<Config> Parameters;
@@ -61,12 +63,12 @@ class SimpleDQM : public art::EDAnalyzer
 	std::string                           address_;
 	std::string                           outputTag_;
 	int                                   freqDQM_, diagLevel_, evtCounter_;
-        bool                                  sendHists_;
+	bool                                  sendHists_;
 	art::ServiceHandle<art::TFileService> tfs;
 	HistoSender*                          histSender_;
 
-        // Simple event counter
-        TH1* hist_;
+	// Simple event counter
+	TH1* hist_;
 };
 }  // namespace ots
 
@@ -81,47 +83,52 @@ ots::SimpleDQM::SimpleDQM(Parameters const& conf)
     , evtCounter_(0)
     , sendHists_(conf().sendHists())
 {
-        histSender_ = (sendHists_) ? new HistoSender(address_, port_) : nullptr;
+	histSender_ = (sendHists_) ? new HistoSender(address_, port_) : nullptr;
 
 	__COUT__ << "[SimpleDQM::" << __func__ << "] Constructor" << std::endl;
 }
 
 void ots::SimpleDQM::beginJob()
 {
-        __COUT__ << "[SimpleDQM::" << __func__ << "] Beginning job" << std::endl;
+	__COUT__ << "[SimpleDQM::" << __func__ << "] Beginning job" << std::endl;
 
 	// Create the histograms of interest
 	art::TFileDirectory dir = tfs->mkdir(outputTag_);
 	hist_ = dir.make<TH1F>("event_count", "Event counter;Bin;Events", 1, 0, 1);
 	hist_->SetLineWidth(2);
-	hist_->SetFillColor(kAzure-9);
-	hist_->SetLineColor(kAzure+2);
+	hist_->SetFillColor(kAzure - 9);
+	hist_->SetLineColor(kAzure + 2);
 	hist_->SetFillStyle(3001);
 }
 
 void ots::SimpleDQM::analyze(art::Event const& event)
 {
-        // fill the histograms of interest
+	// fill the histograms of interest
 	++evtCounter_;
 	hist_->Fill(1);
-        TLOG(TLVL_DEBUG + 20) << "[SimpleDQM::" << __func__ << "] Analyzing event " << evtCounter_ << std::endl;
+	TLOG(TLVL_DEBUG + 20) << "[SimpleDQM::" << __func__ << "] Analyzing event "
+	                      << evtCounter_ << std::endl;
 
 	// Only send histograms at a given frequency
-	if(evtCounter_ % freqDQM_ != 0 || !sendHists_) return;
+	if(evtCounter_ % freqDQM_ != 0 || !sendHists_)
+		return;
 
 	// Add a clone of the histogram to a map of <path/in/output, <list of histograms>>
 	std::map<std::string, std::vector<TH1*>> hists_to_send;
- 	TLOG(TLVL_DEBUG + 5) << "[SimpleDQM::" << __func__ << "] collecting summary histogram "
-			     << hist_->GetName() << std::endl;
+	TLOG(TLVL_DEBUG + 5) << "[SimpleDQM::" << __func__
+	                     << "] collecting summary histogram " << hist_->GetName()
+	                     << std::endl;
 	// histograms can be replaced with ":replace" or added together with ":add" appended to the directory path
-	hists_to_send[outputTag_ + "_summary:replace"].push_back((TH1*) hist_->Clone());
+	hists_to_send[outputTag_ + "_summary:replace"].push_back((TH1*)hist_->Clone());
 
 	// send the histograms to the receiver
 	histSender_->sendHistograms(hists_to_send);
 }
 
-void ots::SimpleDQM::endJob() {
-  __COUT__ << "[SimpleDQM::" << __func__ << "] Ending job, saw " << evtCounter_ << " events\n";
+void ots::SimpleDQM::endJob()
+{
+	__COUT__ << "[SimpleDQM::" << __func__ << "] Ending job, saw " << evtCounter_
+	         << " events\n";
 }
 
 void ots::SimpleDQM::beginRun(const art::Run&) {}
